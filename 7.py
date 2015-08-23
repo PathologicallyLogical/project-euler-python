57b3330e2ae6c6e6ba4c1661f9466b886b47963e
Anina Hitt <PathologicallyLogical@users.noreply.github.com>
2015-08-23 13:40:55 -0700

    Update and rename 7 to 7.py

--- 7.py
+++ 7.py
@@ -15,6 +15,7 @@ print x-2
 
 #started August 22, 2015
 #completed August 22, 2015
+#not very efficient. Fix later.
 
 """
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
